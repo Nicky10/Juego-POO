61e8f2ae77e3baa860cc6b2b84aee8eb5e3f3e39
ADMIN <ADMIN@PORT-JAMES>
2017-05-26 17:19:50 -0500

    Terminado

--- src/juego/Panel.java
+++ src/juego/Panel.java
@@ -1,5 +1,8 @@
 package juego;
 
+
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -10,11 +13,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-/**
- *
- * @author Estudiante
- */
-public class Panel extends JPanel implements ActionListener {
+
+public class Panel extends JPanel  implements ActionListener {
     public Image loadImage(String imageName) {
         ImageIcon ii = new ImageIcon(imageName);
         Image image = ii.getImage();
@@ -23,21 +23,32 @@ public class Panel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        
         if (paradoP == 1) {
             paradoP = 0;
         } else {
             paradoP = 1;
         }
-        t = true;
-//        x+=20;
+        
         caminarP++;
         if (caminarP == 4) {
             caminarP = 0;
         }
+        
+        if(caminarV==1){
+            caminarV=0;
+        }
+        else{
+            caminarV=1;
+        }
+ 
         repaint();
+        
     }
     
     private class TAdapter extends KeyAdapter {
+
+       
         
         @Override
         public void keyReleased(KeyEvent e) {
@@ -45,19 +56,28 @@ public class Panel extends JPanel implements ActionListener {
             switch (key) {
                 case KeyEvent.VK_LEFT:
 
+                     t=false;
                     x-=5;
                     
+                    
+                   
                     break;
                 case KeyEvent.VK_RIGHT:
-
+                    t=false;
                     x+=5;
                     
                     break;
                 case KeyEvent.VK_UP:
                     
-                    while((y+712)!=712)
+                     
+                    while((y+415)!=415){
+                        
                          y++;
-
+                         
+                         }
+                    t=false;
+                    
+                    
                     break;
                 case KeyEvent.VK_DOWN:
 
@@ -72,18 +92,24 @@ public class Panel extends JPanel implements ActionListener {
             int key = e.getKeyCode();
             switch (key) {
                 case KeyEvent.VK_LEFT:
-                    
+              
                     x-=10;
                     
                     break;
                 case KeyEvent.VK_RIGHT:
                     
-                   x+=10;
+                    t=true;
+                    
+                    x+=10;
                     
                     break;
                 case KeyEvent.VK_UP:
+
+                    t=true;
                     
-                     y-=100;
+                     y-=150;
+                     Puntuacion+=10;
+                     timer.start();
                      
 
                     break;
@@ -98,19 +124,26 @@ public class Panel extends JPanel implements ActionListener {
     }
     
     private int caminarP;
+    private int caminarV;
     private int paradoP;
     private int BackGroundX;
     private int y;
     private int x;
+    private int xVillano;
     private Timer timer;
-    boolean t;
+    boolean t=false;
+    
+    private int Puntuacion=0;
+    
+    
     
     public Panel() {
         addKeyListener(new TAdapter());
         setFocusable(true);
         this.x = 0;
+        this.xVillano = 1000;
         this.y = 0;
-        this.timer = new Timer(90, this);
+        this.timer = new Timer(150, this);
         timer.start();
     }
     
@@ -121,12 +154,41 @@ public class Panel extends JPanel implements ActionListener {
             g.drawImage(loadImage("red_background.png"), (BackGroundX + i), 0, this);
         }
         for (int i = 0; i < 2201; i+=112) {
-            g.drawImage(loadImage("ground_loop.png"), (BackGroundX + i), 712, this);
+            g.drawImage(loadImage("ground_loop.png"), (BackGroundX + i), 412, this);
         }
+        Font font=new Font("Tahoma",1,50);
+        g.setFont(font);
+        g.drawString("Puntuacion: "+Puntuacion, 15, 40);
         
-            g.drawImage(loadImage("walking.png"), (x + 50), (y+578), (x + 168), (y+712), (124 * caminarP-20), 0, ((124 * caminarP) + 98), 134, this);
+        if(t==false){
+            g.drawImage(loadImage("standing.png"), (x + 50), 281, (x + 190), 415, (142 * paradoP), 0, ((142 * paradoP) + 142), 133, this);
+            
+            }
+           
+       
+            if(t==true){
+            g.drawImage(loadImage("walking.png"), (x + 50), (y+281), (x + 168), (y+415), (124 * caminarP-20), 0, ((124 * caminarP) + 98), 134, this);
+            
+        }
+        g.drawImage(loadImage("enemy_run.png"), xVillano, 314, (xVillano+105),415,(105*caminarV),0,((105*caminarV)+105),101, this);
+        
+          for(int i=50;i<1000;i=i+300){
+              
+        g.drawImage(loadImage("coin.png"), i, 100, 55,55, this);
+        
+          }
+          
+          for(int i=120;i<1200;i=i+300){
+              
+        g.drawImage(loadImage("stone.png"), i, 100, 55,59, this);
+        
+          }
+          
+          
+          
     }
     
-    
-    
 }
+
+    
+
